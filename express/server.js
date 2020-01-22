88dcd70989a3ec1d33fd3c78a4d47c205b4d22b7
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-01-23 00:53:47 +0330

    add /mail/send route

--- express/server.js
+++ express/server.js
@@ -13,6 +13,7 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.post('/mail/send', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda

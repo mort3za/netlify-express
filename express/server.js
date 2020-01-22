923783687263302a607e9b4ddc4c42e946cd312d
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-01-23 02:30:56 +0330

    dynamic data

--- express/server.js
+++ express/server.js
@@ -4,7 +4,10 @@ const path = require("path");
 const serverless = require("serverless-http");
 const app = express();
 const bodyParser = require("body-parser");
-const emailSender = require("./utils/mailgun");
+const { emailSender } = require("./utils/mailgun");
+
+// base url for all express routes is: /.netlify/functions/server
+// e.g. /.netlify/functions/server/mail/send
 
 const router = express.Router();
 router.get("/", (req, res) => {
@@ -12,13 +15,26 @@ router.get("/", (req, res) => {
   res.write("<h1>Hello from Express.js!</h1>");
   res.end();
 });
-router.get("/another", (req, res) => res.json({ salamAnother: 1 }));
+
 router.post("/", (req, res) => res.json({ postBody: req.body }));
 router.post("/mail/send", (req, res) => {
-  emailSender()
+  let result
+  if (typeof req.body == 'object') {
+    result = emailSender(req.body);
+  }
+  
+  if (
+    typeof result == "object" &&
+    (result.message || "").toLowerCase().includes("Queued")
+  ) {
+    res.status(200).end();
+  } else {
+    res.status(400).end();
+  }
 });
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/.netlify/functions/server", router); // path must route to lambda
 app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
--- express/utils/mailgun.js
+++ express/utils/mailgun.js
@@ -1,17 +1,18 @@
 var api_key = process.env.MAILGUN_API_KEY;
 const domain = process.env.MAILGUN_DOMAIN;
 var mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
+const email_from = process.env.EMAIL_FROM;
 
-exports.emailSender = function() {
+exports.emailSender = function({ email_to, email_subject, email_text }) {
   const data = {
-    from: `Mailgun Sandbox <postmaster@${domain}>`,
-    to: "m.ziaeemehr@gmail.com",
-    subject: "Hello from MG",
-    text: "Testing some Mailgun awesomness!"
+    from: email_from,
+    to: email_to,
+    subject: email_subject,
+    text: email_text
   };
 
   mailgun.messages().send(data, function(error, body) {
     console.log(body);
     return body;
   });
-}
+};

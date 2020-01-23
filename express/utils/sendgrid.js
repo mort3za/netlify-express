df1061fa7790430ec6244d06871e16c9204c95de
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-01-23 19:31:36 +0330

    update.

--- express/utils/sendgrid.js
+++ express/utils/sendgrid.js
@@ -1,7 +1,10 @@
 // https://github.com/sendgrid/sendgrid-nodejs/blob/master/use-cases/transactional-templates.md
 const sgMail = require("@sendgrid/mail");
-const email_from = process.env.EMAIL_FROM;
 const api_key = process.env.SENDGRID_API_KEY;
+const email_from = process.env.EMAIL_FROM;
+
+console.log('keeeeeeeeeeeeeeeeeeeeeeeeeeeey: ', api_key.substring(0, 5));
+
 
 exports.emailSender = function({
   email_to,
@@ -9,20 +12,19 @@ exports.emailSender = function({
   email_text,
   template_data
 }) {
-  sgMail.setApiKey(api_key);
-  const msg = {
-    to: email_to,
-    from: email_from,
-    subject: email_subject,
-    text: email_text,
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>"
-    // templateId: "d-9a3ef6bc785244159ea9ab2f8bf2c8a6",
-    // dynamic_template_data: {
-    //   subject: "Testing Templates",
-    //   name: "Some One",
-    //   noun: "This_is_nouN"
-    // }
-  };
+  // const msg = {
+  //   to: email_to,
+  //   from: email_from,
+  //   subject: email_subject,
+  //   text: email_text,
+  //   html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+  //   // templateId: "d-9a3ef6bc785244159ea9ab2f8bf2c8a6",
+  //   // dynamic_template_data: {
+  //   //   subject: "Testing Templates",
+  //   //   name: "Some One",
+  //   //   noun: "This_is_nouN"
+  //   // }
+  // };
 
   return new Promise((resolve, reject) => {
     try {
@@ -42,6 +44,7 @@ exports.emailSender = function({
         text: "Hello plain world!",
         html: "<p>Hello <b>HTML</b> world!</p>"
       };
+      sgMail.setApiKey(api_key);
       sgMail.send(msg);
     } catch (error) {
       reject({ message: "mailer service has a problem" });

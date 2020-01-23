beed0d59fd66e6e179d957db1ef76db0972c784e
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-01-23 22:51:51 +0330

    update.

--- express/utils/sendgrid.js
+++ express/utils/sendgrid.js
@@ -1,11 +1,10 @@
-// https://github.com/sendgrid/sendgrid-nodejs/blob/master/use-cases/transactional-templates.md
 const sgMail = require("@sendgrid/mail");
 const api_key = process.env.SENDGRID_API_KEY;
 const email_from = process.env.EMAIL_FROM;
 
 console.log(
-  `apikey: ${api_key.substring(0, 5)}...${api_key.substring(
-    api_key.length - 2
+  `apikey: ${api_key.substring(0, 4)}...${api_key.substring(
+    api_key.length - 1
   )}`
 );
 
@@ -13,20 +12,18 @@ exports.emailSender = async function({
   email_to,
   email_subject,
   email_text,
+  email_html,
+  template_id,
   template_data
 }) {
   const msg = {
     to: email_to,
     from: email_from,
     subject: email_subject,
-    text: email_text
-    // html: "<strong>and easy to do anywhere, even with Node.js</strong>"
-    // templateId: "d-9a3ef6bc785244159ea9ab2f8bf2c8a6",
-    // dynamic_template_data: {
-    //   subject: "Testing Templates",
-    //   name: "Some One",
-    //   noun: "This_is_nouN"
-    // }
+    text: email_text,
+    html: email_html,
+    templateId: template_id,
+    dynamic_template_data: template_data
   };
 
   try {
@@ -38,8 +35,9 @@ exports.emailSender = async function({
       error.response && error.response.body && error.response.body.errors;
     console.log("error =======>", errors);
     return {
+      error: true,
       message:
-        (errors.length > 0 && errors[0].message) ||
+        (errors && errors.length > 0 && errors[0].message) ||
         "mailer service has a problem"
     };
   }
